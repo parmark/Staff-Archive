9b8b049069b8171a155b1f6159a51f0923f73680
Kevin Parmar <parmarkevinb@gmail.com>
2020-04-01 16:01:33 -0400

    Edited App.js to display a list of staff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from "react";
+import staff from "./staff.json"
+import StaffEntry from "./components/StaffEntry"
 
-function App() {
-    return <div />
+class App extends React.Component {
+    state = {
+        staff: staff
+    }
+
+    render() {
+        return (
+            <div>
+                <table>
+                    {/* Table Headers */}
+                    <thead>
+                        <th>Picture</th>
+                        <th>Name</th>
+                        <th>Age</th>
+                    </thead>
+
+                    {/* Table Entries */}
+                    <tbody>
+                        {this.state.staff.map(staff => 
+                            <StaffEntry
+                                first={staff.name.first}
+                                last={staff.name.last}
+                                age={staff.dob.age}
+                                picture={staff.picture.thumbnail}
+                                key={staff.login.uuid}
+                            />)}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
 
 export default App;
