81129a5e6568ff559226ed4a639e240ecd5a9a6f
Kevin Parmar <parmarkevinb@gmail.com>
2020-04-01 16:53:09 -0400

    Added App.css and styled page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import staff from "./staff.json"
 import StaffEntry from "./components/StaffEntry"
 import Header from "./components/Header"
+import "./App.css"
 
 class App extends React.Component {
     state = {
@@ -10,9 +11,9 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="container">
                 <Header />
-                <table>
+                <table className="responsive-table highlight">
                     {/* Table Headers */}
                     <thead>
                         <th>Picture</th>
